3e7b96d9e3004c91168401a6e7e162d5da7d5de3
Jared Whalen <jwhalen@philly.com>
2018-10-17 14:16:06 -0400

    top resize

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+  $(window).on("load resize", function(e) {
+    if ($(window).width() > 800) {
+      var h = $('.header').height(),
+        w = $(window).height(),
+        v = w - h;
+      $(".headerIntro.top").css({
+        "height": v
+      })
+    } else {
+      $(".headerIntro.top").css({
+        "height": "auto"
+      })
+    }
+  });
+
 });
